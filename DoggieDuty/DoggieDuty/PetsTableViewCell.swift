427c84043f2e7f2d882329c683d3584c0937967b
Hailey Cassidy <HaileyCassidy@MacBook-Pro-5.local>
2019-04-29 00:09:43 -0500

    pet view set up and walkthrough videos added

---
+++ DoggieDuty/DoggieDuty/PetsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PetsTableViewCell.swift
+//  DoggieDuty
+//
+//  Created by Hailey Cassidy on 4/28/19.
+//  Copyright © 2019 Amanda Jackson. All rights reserved.
+//
+
+import UIKit
+
+class PetsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
